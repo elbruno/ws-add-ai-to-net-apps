0970aa80feb1338bde888c439ed45f900769da35
Bruno Capuano <bcapuano@gmail.com>
2024-07-15 10:22:28 -0400

    ready for event

--- src/chat-server/Controllers/ChatController.cs
+++ src/chat-server/Controllers/ChatController.cs
@@ -28,16 +28,11 @@ public class ChatController : ControllerBase
 
     private ChatHistory _chatHistory;
 
-    private IChatCompletionService _chatCompletionService;
-    private ISemanticTextMemory _memory;
-
-    public ChatController(ILogger<ChatController> logger, IConfiguration config, ChatHistory chatHistory, IChatCompletionService chatCompletionService, ISemanticTextMemory semanticMemory)
+    public ChatController(ILogger<ChatController> logger, IConfiguration config, ChatHistory chatHistory)
     {
         _logger = logger;
-        _config = config;
         _chatHistory = chatHistory;
-        _chatCompletionService = chatCompletionService;
-        _memory = semanticMemory;
+        _config = config;
     }
 
     // POST api/<ChatController>
@@ -48,88 +43,21 @@ public class ChatController : ControllerBase
 
         var response = new Response
         {
-            Author = _config["Author"]
+            Author = string.IsNullOrEmpty(_config["Author"]) ? "chatbot" : _config["Author"]
         };
+        // complete chat history
+        _chatHistory.AddUserMessage(question.UserQuestion);
 
         // get response
         var stopwatch = new Stopwatch();
         stopwatch.Start();
-
-        var result = await SearchResultInMemory(question);
-        if (string.IsNullOrEmpty(result))
-        {
-            // complete chat history
-            _chatHistory.AddUserMessage(question.UserQuestion);
-
-            response.Author = "Azure GPT-4o";
-            var resultResponse = await _chatCompletionService.GetChatMessageContentsAsync(_chatHistory);
-            result = resultResponse[^1].Content;
-            await AddItemToMemory(question, result);
-            response.FromCache = false;
-        }
-        else
-        {
-            response.Author += " [Cache]";
-            response.FromCache = true;
-        }
-
-        response.QuestionResponse = result;
-
-        // calculate elapsed time
+        var chatResponse = $" Your question [{question.UserQuestion}] is {question.UserQuestion.Length} chars long.";
         stopwatch.Stop();
+        response.QuestionResponse = chatResponse;
         response.ElapsedTime = stopwatch.Elapsed;
 
+        // return response
         _logger.LogInformation($"Response: {response}");
         return response;
-    }
-
-    async Task AddItemToMemory(Question question, string questionAnswer)
-    {
-        int chunkSize = 6000;
-        List<string> chunks = new List<string>();
-        for (int i = 0; i < question.UserQuestion.Length; i += chunkSize)
-        {
-            string chunk = question.UserQuestion.Substring(i, Math.Min(chunkSize, question.UserQuestion.Length - i));
-            string key = question.UserQuestion + "-" + i;
-
-            // process key to only contain letters, digits, underscore (_), dash (-), or equal sign (=)
-            key = new string(key.Where(c => char.IsLetterOrDigit(c) || c == '_' || c == '-' || c == '=').ToArray());
-
-            await _memory.SaveInformationAsync(
-                collection: GetMemoryCollectionName(question),
-                text: chunk,
-                id: key,
-                description: questionAnswer);
-
-            _logger.LogInformation($"Saved chunk: {key} - {chunk}");
-        }
-    }
-
-    async Task<string> SearchResultInMemory(Question question)
-    {
-        var returnValue = string.Empty;
-
-        var collectionName = GetMemoryCollectionName(question);
-
-        // search in memory
-        var response = await _memory.SearchAsync(
-            collectionName,
-            question.UserQuestion,
-            withEmbeddings: true,
-            limit: 1).FirstOrDefaultAsync();
-        if (response != null)
-        {
-            _logger.LogInformation($"{question.UserQuestion} >> ID: {response?.Metadata.Id} - Description: {response?.Metadata.Description} - Relevance: {response.Relevance} - Is Reference: {response?.Metadata.IsReference}");
-            if (response.Relevance > 0.9)
-            {
-                returnValue = response?.Metadata.Description;
-            }
-        }
-        return returnValue;
-    }
-
-    string GetMemoryCollectionName(Question question)
-    {
-        return $"user-{question.UserName}-chat";
     }
 }
